6024965c62caffb07c4da00b3c194af047bb1e17
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-07-10 15:30:53 -0400

    Create MultipleOfThree.cpp

---
+++ MultipleOfThree.cpp
@@ -0,0 +1,23 @@
+/*  name: jianxin zhu
+    date: july/10/2020
+ */
+ 
+ #include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	for (unsigned int i = 3; i <= 50; i++)
+	{
+		cout << i * 3 << " ";
+
+		if (i % 10 == 0)
+		{
+			cout << endl;
+		}
+	}
+
+}
