cf330b7e8bdcef41fa3bef95ff19e00554eb1d23
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-07-11 20:06:26 -0400

    Update CalculatePi.cpp

--- CalculatePi.cpp
+++ CalculatePi.cpp
@@ -2,21 +2,21 @@
      date: july/10/2020
  */
  
- #include<iostream>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
-	double Pi = 4;
-	double sum = 0.0;
 	int sign = 4;
+	double Pi = 4.0;
 
 	for (int i = 1; i <= 100; i++)
 	{
-		sum = sum + sign / (2.0 * i + 1.0);
+		Pi = Pi - (sign) / (2.0 * i + 1.0);
 		sign = sign * (-1);
 	}
 
-	cout << "The Pi is: " << (Pi - sum) << endl;
+	cout << "The Pi is: " << Pi << endl;
+
 }
